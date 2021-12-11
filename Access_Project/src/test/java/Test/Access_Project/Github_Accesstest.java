319b1a2d5d2aa8eac00ead4184f8a42338f35d3a
Mahendrakar <Mahendrakar@LAPTOP-DPHUNK0U>
2021-12-11 16:07:47 +0530

    Hey Access

---
+++ Access_Project/src/test/java/Test/Access_Project/Github_Accesstest.java
@@ -0,0 +1,27 @@
+package Test.Access_Project;
+
+import org.testng.annotations.Test;
+
+public class Github_Accesstest 
+{
+
+	public class TC01Test
+	{
+		    @Test
+			public void cool()
+			{
+				System.out.println("Coolllllllllllllllllllll...!");
+			}
+			
+			@Test
+			public void chill()
+			{
+				System.out.println("Chilllllllllllllllllllll...!");
+			}
+
+	}
+
+
+
+
+}
